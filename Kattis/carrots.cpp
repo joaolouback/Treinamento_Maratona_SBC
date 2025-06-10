eb83a82351a4e62862fb507ba709532a2d49ea2b
joaolouback <jpestevaoloubackbr@gmail.com>
2025-06-10 00:24:36 -0300

    carrots

---
+++ Kattis/carrots.cpp
@@ -0,0 +1,21 @@
+// URL: https://open.kattis.com/problems/carrots
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int problemas, pessoas, count;
+    
+    string descricao;
+
+    cin >> pessoas >> problemas; 
+
+    while(count--){
+        cin >> descricao; 
+    }
+
+    cout << problemas << endl; 
+
+    return 0; 
+}
