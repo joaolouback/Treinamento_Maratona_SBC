d3afd5d1c57d0952584a1e320cc021a68606006e
joaolouback <jpestevaoloubackbr@gmail.com>
2025-06-10 00:56:43 -0300

    thelastproblem

---
+++ Kattis/thelastproblem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+
+    getline(cin, name);
+
+    cout << "Thank you, " << name << ", and farewell!" << endl;
+    
+    return 0;
+}
