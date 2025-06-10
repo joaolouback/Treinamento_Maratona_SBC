bd44db0ac31322e726592d390140305ceb2c71ac
joaolouback <jpestevaoloubackbr@gmail.com>
2025-06-10 00:43:41 -0300

    r2

---
+++ Kattis/r2.cpp
@@ -0,0 +1,18 @@
+// URL: https://open.kattis.com/problems/r2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int B, media;
+    
+    cin >> B;
+    cin >> media;
+    
+    cout << 2 * media - B << endl;
+    
+   return 0;
+}
+
+//Achart o valor de C de acordo com a media e o valor de B
