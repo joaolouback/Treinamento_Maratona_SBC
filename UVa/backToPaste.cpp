6e9b96ee88daa0c9aec3f43b11e01a0e1c12fb0a
joaolouback <jpestevaoloubackbr@gmail.com>
2025-06-10 00:09:55 -0300

    Exercicio Paste

---
+++ UVa/backToPaste.cpp
@@ -0,0 +1,11 @@
+// URL: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=866&page=show_problem&problem=4913
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "October 30, 2015 Friday" << endl; 
+    return 0;
+}
