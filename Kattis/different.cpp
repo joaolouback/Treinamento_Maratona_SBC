bcaa33356702aac32868baa8866b386c5c7fd32d
joaolouback <jpestevaoloubackbr@gmail.com>
2025-06-10 21:25:12 -0300

    different

---
+++ Kattis/different.cpp
@@ -0,0 +1,22 @@
+//URL: 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  long int N1, N2, dif;
+  
+  while (  cin >> N1 >> N2){
+      
+      dif  = N1 - N2; 
+      
+      if (dif < 0) { 
+        dif = dif * (-1); // Converte o número negativo para positivo
+    }
+    
+    cout << dif << endl; 
+  }
+  return 0;
+}
